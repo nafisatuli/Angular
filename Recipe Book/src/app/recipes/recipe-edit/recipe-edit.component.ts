e0e0f949bd0416e62111aed65218aaabc1f8f3fb
Nafisa Hasan <nafisahasan98@gmail.com>
2020-07-12 23:33:23 +0600

    creating the base of recipe edit form

--- Recipe Book/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ Recipe Book/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { FormGroup, FormControl } from '@angular/forms';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -12,12 +14,37 @@ export class RecipeEditComponent implements OnInit {
 
   id: number;
   editMode = false;
-  constructor(private route: ActivatedRoute) {}
+
+  recipeForm: FormGroup;
+  constructor(
+    private route: ActivatedRoute,
+    private recipeService: RecipeService
+  ) {}
   //retrieve the id
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
       this.id = +params['id'];
       this.editMode = params['id'] != null; //does it have the id
+      this.initForm();
+    });
+  }
+  //initialize our recipe edit form
+  //this method should be called when our route params will change bcz that indicate we reload the page
+  private initForm() {
+    let recipeName = '';
+    let recipeImgPath = '';
+    let recipeDescription = '';
+    if (this.editMode) {
+      const recipe = this.recipeService.getSingleRecipe(this.id); //fetch recipe
+      recipeName = recipe.name;
+      recipeImgPath = recipe.imgPath;
+      recipeDescription = recipe.description;
+    }
+
+    this.recipeForm = new FormGroup({
+      name: new FormControl(recipeName),
+      imagePath: new FormControl(recipeImgPath),
+      description: new FormControl(recipeDescription),
     });
   }
 }
