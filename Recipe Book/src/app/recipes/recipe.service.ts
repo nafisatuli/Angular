a54604df36ce866c944f7a13c0c49110d395d812
Nafisa Hasan <nafisahasan98@gmail.com>
2020-08-11 15:08:37 +0600

    fix bugs and finish the project

--- Recipe Book/src/app/recipes/recipe.service.ts
+++ Recipe Book/src/app/recipes/recipe.service.ts
@@ -11,20 +11,21 @@ export class RecipeService {
   // recipeSelected = new Subject<Recipe>(); //it is no need after adding routing
 
   //array of recipe in the recipe service
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Stuffed Potatoes',
-      'A super tasty dish!',
-      'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2019/10/4/0/FNM_110119-Name-This-Dish-Stuffed-Potatoes_s4x3.jpg.rend.hgtvcom.826.620.suffix/1570214935058.jpeg',
-      [new Ingredient('Meat', 1), new Ingredient('French fry', 20)]
-    ),
-    new Recipe(
-      'Tandoori Chicken',
-      'Smells great and tastes so exotic!',
-      'https://www.mozismenu.com/wp-content/uploads/2018/03/Tandoori-Chicken-0.jpg',
-      [new Ingredient('Naan', 2), new Ingredient('Garlic sauce', 1)]
-    ),
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Stuffed Potatoes',
+  //     'A super tasty dish!',
+  //     'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2019/10/4/0/FNM_110119-Name-This-Dish-Stuffed-Potatoes_s4x3.jpg.rend.hgtvcom.826.620.suffix/1570214935058.jpeg',
+  //     [new Ingredient('Meat', 1), new Ingredient('French fry', 20)]
+  //   ),
+  //   new Recipe(
+  //     'Tandoori Chicken',
+  //     'Smells great and tastes so exotic!',
+  //     'https://www.mozismenu.com/wp-content/uploads/2018/03/Tandoori-Chicken-0.jpg',
+  //     [new Ingredient('Naan', 2), new Ingredient('Garlic sauce', 1)]
+  //   ),
+  // ];
+  private recipes: Recipe[] = [];
 
   constructor(private slService: ShoppingListService) {}
 
